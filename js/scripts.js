027a2bdc207154e54cacd572947520ce3a68913e
diehnmanuel <100957408+diehnmanuel@users.noreply.github.com>
2022-07-25 21:24:41 -0300

    Update scripts.js
    change script page

--- js/scripts.js
+++ js/scripts.js
@@ -216,9 +216,9 @@ $(document).ready(function () {
 
         if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
-            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
+            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite codee is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbzUqz44wOat0DiGjRV1gUnRf4HRqlRARWggjvHKWvqniP7eVDG-/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbwfPTYnHqaEvk7cls-KXfTsxbyhemVXltwmv-3cGAJWbXQjxTMdZOU7rKsxKcuArk9ypw/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
