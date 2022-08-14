630d14d9e22810c89a8b94898573db41c4244804
Manuel Diehn <diehn.manuel@bcg.com>
2022-08-15 00:13:47 +0200

    change code

--- js/scripts.js
+++ js/scripts.js
@@ -214,11 +214,11 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a second!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
+        if (MD5($('#invite_code').val()) !== 'a4e23b2609285cfd99de6d9832f21df1'
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyMeI3Zp2w6S7oR0WT95zlD300PgRVKUm7mmWqvVNvpBFpam9GNgF8fngCpmUJgAs4mgw/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbyyM36W-gfO0r6Lep1nkDPVrj-3URoN_K5u1ODWQMFD18fqOQ_NNuaVnKBNa4rj-KCj3A/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
